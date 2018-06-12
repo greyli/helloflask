6ed1524801d0b6a584062e30984a524f6229da0e
Grey Li <withlihui@gmail.com>
2018-06-12 16:31:08 +0800

    Add assets application for chapter 13

---
+++ demos/assets/app.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+from flask_assets import Environment, Bundle
+from flask_ckeditor import CKEditor
+
+
+app = Flask(__name__)
+app.secret_key = 'dev key'
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+assets = Environment(app)
+ckeditor = CKEditor(app)
+
+css = Bundle('css/bootstrap.min.css',
+             'css/bootstrap.css',
+             'css/dropzone.min.css',
+             'css/jquery.Jcrop.min.css',
+             'css/style.css',
+             filters='cssmin', output='gen/packed.css')
+
+js = Bundle('js/jquery.min.js',
+            'js/popper.min.js',
+            'js/bootstrap.min.js',
+            'js/bootstrap.js',
+            'js/moment-with-locales.min.js',
+            'js/dropzone.min.js',
+            'js/jquery.Jcrop.min.js',
+            filters='jsmin', output='gen/packed.js')
+
+assets.register('js_all', js)
+assets.register('css_all', css)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/foo')
+def unoptimized():
+    return render_template('unoptimized.html')
+
+
+@app.route('/bar')
+def optimized():
+    return render_template('optimized.html')
---
+++ demos/assets/static/ckeditor/build-config.js
@@ -0,0 +1,104 @@
+﻿/**
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) http://ckeditor.com/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) http://ckeditor.com/builder/65b7556a252134a633858afb266aefb6
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) http://ckeditor.com/builder/download/65b7556a252134a633858afb266aefb6
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono-lisa',
+	preset: 'standard',
+	ignore: [
+		'.DS_Store',
+		'.bender',
+		'.editorconfig',
+		'.gitattributes',
+		'.gitignore',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'.mailmap',
+		'.travis.yml',
+		'bender-err.log',
+		'bender-out.log',
+		'bender.ci.js',
+		'bender.js',
+		'dev',
+		'gruntfile.js',
+		'less',
+		'node_modules',
+		'package.json',
+		'tests'
+	],
+	plugins : {
+		'a11yhelp' : 1,
+		'about' : 1,
+		'basicstyles' : 1,
+		'blockquote' : 1,
+		'codesnippet' : 1,
+		'contextmenu' : 1,
+		'elementspath' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'filebrowser' : 1,
+		'floatingspace' : 1,
+		'format' : 1,
+		'horizontalrule' : 1,
+		'htmlwriter' : 1,
+		'image' : 1,
+		'indentlist' : 1,
+		'link' : 1,
+		'list' : 1,
+		'magicline' : 1,
+		'markdown' : 1,
+		'maximize' : 1,
+		'pastefromword' : 1,
+		'pastetext' : 1,
+		'removeformat' : 1,
+		'resize' : 1,
+		'scayt' : 1,
+		'showborders' : 1,
+		'sourcearea' : 1,
+		'specialchar' : 1,
+		'stylescombo' : 1,
+		'tab' : 1,
+		'table' : 1,
+		'tableselection' : 1,
+		'tabletools' : 1,
+		'toolbar' : 1,
+		'undo' : 1,
+		'uploadimage' : 1,
+		'wsc' : 1,
+		'wysiwygarea' : 1
+	},
+	languages : {
+		'de' : 1,
+		'en' : 1,
+		'es' : 1,
+		'fr' : 1,
+		'ja' : 1,
+		'ko' : 1,
+		'zh' : 1,
+		'zh-cn' : 1
+	}
+};
