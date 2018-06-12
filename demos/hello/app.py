9fad70d6bda3393963ad18fd7bb45315a683860d
Grey Li <withlihui@gmail.com>
2018-06-12 10:43:00 +0800

    Add hello application for chapter 1

---
+++ demos/hello/app.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import click
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# the minimal Flask application
+@app.route('/')
+def index():
+    return '<h1>Hello, World!</h1>'
+
+
+# bind multiple URL for one view function
+@app.route('/hi')
+@app.route('/hello')
+def say_hello():
+    return '<h1>Hello, Flask!</h1>'
+
+
+# dynamic route, URL variable default
+@app.route('/greet', defaults={'name': 'Programmer'})
+@app.route('/greet/<name>')
+def greet(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
+# custom flask cli command
+@app.cli.command()
+def hello():
+    """Just say hello."""
+    click.echo('Hello, Human!')
